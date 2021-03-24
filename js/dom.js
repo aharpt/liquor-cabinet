7bcebe592eab342df17bc37ed7d1d0e3c135fa25
Aaron Harpt <a169h161@ku.edu>
2021-03-23 20:59:06 -0500

    Add code comments

--- js/dom.js
+++ js/dom.js
@@ -1,11 +1,16 @@
+// drinksChosen holds all the drinks that the user selects
 let drinksChosen = [];
 
+// "$" is for JavaScript libary jQuery
+
+// click handler
 $(".list-group-item").click(function() {
   drinksChosen.push(this.innerText);
   $(this).addClass("active");
   $(this).addClass("disabled");
 });
 
+// hover handler
 $(".list-group-item").hover(function() {
   $(this).css("cursor", "pointer");
 });
