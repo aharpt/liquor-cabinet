6e30f70e742ecd8ac64192b47971416347349880
Aaron Harpt <a169h161@ku.edu>
2021-04-21 11:24:29 -0500

    Get recipe testing working

--- js/tests.js
+++ js/tests.js
@@ -1,4 +1,14 @@
 // when "Run Tests" button is clicked
 document.querySelector(".tests").addEventListener("click", function() {
+  // Testing "See Full Recipe" button
   $(".liquor-options li").trigger("click");
+
+  // simulating click to first "See Full Recipe" button
+  $(document.querySelector(".recipe-button")).trigger("click");
+
+  if (document.querySelector(".recipe-button").style.display == "none") {
+    console.log("Recipe Button Shows Full Recipe when clicked : Passed");
+  } else {
+    console.log("Recipe Button Shows Full Recipe when clicked : Failed");
+  }
 });
