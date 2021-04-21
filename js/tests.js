ae784cb968c1ea0cc4bfc35c2f35951c1bbd1f6f
Aaron Harpt <a169h161@ku.edu>
2021-04-21 15:04:19 -0500

    Added to works cited

--- js/tests.js
+++ js/tests.js
@@ -1,4 +1,9 @@
 // when "Run Tests" button is clicked
+
+let triggerClear = () => {
+  $("#clearButton").trigger("click");
+};
+
 document.querySelector(".tests").addEventListener("click", function() {
   // Testing "See Full Recipe" button
   $(".liquor-options li").trigger("click");
@@ -11,4 +16,6 @@ document.querySelector(".tests").addEventListener("click", function() {
   } else {
     console.log("Recipe Button Shows Full Recipe when clicked : Failed");
   }
+
+  setTimeout(triggerClear, 5000);
 });
