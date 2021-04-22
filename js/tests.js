3ec0f53fc3bdf814bd8c719338492c03b9b71b68
Eva Morrison <eva.morrison33@ku.edu>
2021-04-21 20:40:00 -0500

    update tests

--- js/tests.js
+++ js/tests.js
@@ -1,4 +1,6 @@
 // when "Run Tests" button is clicked
 document.querySelector(".tests").addEventListener("click", function() {
   $(".liquor-options li").trigger("click");
+  console.log(document.getElementsByClassName("list-group-item active disabled")[1] );
 });
+
